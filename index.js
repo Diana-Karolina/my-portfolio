30e2844f905944171aa079e7865c27bdbac76a0a
Diana-Karolina <ortegadianakarolina@gmail.com>
2023-09-29 17:24:07 -0500

    .

--- index.js
+++ index.js
@@ -6,3 +6,12 @@ menu.onClick = () => {
     menu.classList.toggle('bx-x');
     navbar.classList.toggle('open');
 };
+
+const myScroll = ScrollReveal({
+    distancia: '40px',
+    duracion: 2500,
+    reset:True 
+});
+
+myScroll.reveal('.logo',{delay:200, origin:'left'});
+myScroll.reveal('.navbar',{delay:400, origin:'top'});
