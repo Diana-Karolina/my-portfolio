1be2441160a91892d7dd9c8aee031445aaf2d14e
Diana-Karolina <ortegadianakarolina@gmail.com>
2023-10-03 18:44:58 -0500

    add info

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
-
-let menu = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
-
-menu.onClick = () => {
-    menu.classList.toggle('bx-x');
-    navbar.classList.toggle('open');
-};
-
 const myScroll = ScrollReveal({
     distancia: '40px',
     duracion: 2500,
-    reset:True 
+    reset:true 
 });
 
 myScroll.reveal('.logo',{delay:50, origin:'left'});
